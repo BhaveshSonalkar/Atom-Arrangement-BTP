08cd0ebe351f6acd0fcf60c9b354bf7187629944
Bhavesh <bhaveshsonalkar@gmail.com>
2024-05-07 13:17:37 +0530

    small fix

--- generate_input.py
+++ generate_input.py
@@ -1,14 +1,17 @@
 import os
 import random
 
-def generate_matrix(rows, cols):
-    matrix = [[random.randint(0, 1) for _ in range(cols)] for _ in range(rows)]
-    return matrix
+
+def generate_matrix(number_of_rows, number_of_columns):
+	matrix = [[random.randint(0, 1) for _ in range(number_of_columns)] for _ in range(number_of_rows)]
+	return matrix
+
 
 def write_matrix(matrix, file_name):
-    with open(file_name, 'w') as file:
-        for row in matrix:
-            file.write(",".join(map(str, row)) + "\n")
+	with open(file_name, 'w') as file:
+		for row in matrix:
+			file.write(",".join(map(str, row)) + "\n")
+
 
 # Define the dimensions for the matrices
 rows = cols = 10
@@ -16,7 +19,7 @@ rows = cols = 10
 # Create the 'inputs' directory if it doesn't exist
 input_dir = 'inputs'
 if not os.path.exists(input_dir):
-    os.makedirs(input_dir)
+	os.makedirs(input_dir)
 
 # Generate input matrix
 input_matrix = generate_matrix(rows, cols)
